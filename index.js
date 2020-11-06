8e6a3778ae87fb1ff77e9d5d0ad549eb24e42daa
nils.obi10 <you@example.com>
2020-11-06 20:09:07 +0100

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/demo/install/sw.js')
+           .register('/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
