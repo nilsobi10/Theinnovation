a1b4392ec1535cb14f182852f49e14efaa6b11a7
nils.obi10 <you@example.com>
2021-01-15 19:05:12 +0100

    m

--- pages/wetter.js
+++ pages/wetter.js
@@ -2,7 +2,7 @@
 var wetterpage = document.querySelector("#klima");
 function ini_wetter() {
   startload();
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=Erfurt&appid=f110ed3e8279b2f034514ff87e0a387f", {
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=50.975654&lon=10.948052&appid=f110ed3e8279b2f034514ff87e0a387f", {
       method: 'POST',
       headers: {
 
@@ -16,7 +16,7 @@ function ini_wetter() {
       var speed = parseInt(d.wind.speed.toString());    //Windgeschwindigkeit
 
       speed=speed*3.6;
-      speed=speed+" km/h"
+      speed='Windgeschwindigkeit: '+speed+" km/h";
       //speed= 'Die aktuelle Windgeschwindigkeit beträt: '+speed;
 
       var deg = parseInt(d.wind.deg.toString());          //Windrichtung
@@ -30,10 +30,16 @@ function ini_wetter() {
       else if(deg <= 112.5+45*5){deg='Nordwesten'}
       else if(deg <= 112.5+45*6){deg='Norden'};
       //deg='Die aktuelle Windrichtung beträgt: '+deg;
+      deg='Windrichtung: '+deg;
+
+      var temp = parseFloat(d.main.temp.toString());
+      temp-=273.15;
+      temp=Math.round(temp*10)/10;
+      temp='Temperatur: '+temp+' °C';
 
       var pic='<img class="bild" src="picture/bewölkt.png" draggable="false">'   //Wetterereignisserkennung
 
-      var out='<div class=wetter>'+pic+deg+speed+'</div>';
+      var out='<div class=wetter>'+'Erfurt-Bindersleben'+'<br>'+pic+'<br>'+deg+'<br>'+speed+'<br>'+temp+'</div>';
 
       wetterpage.innerHTML = out;
 
