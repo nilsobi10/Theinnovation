cfaf3cf92bd91071802acb3fa16985a490ada5c2
nils.obi10 <you@example.com>
2021-01-30 17:09:39 +0100

    m

--- pages/status.js
+++ pages/status.js
@@ -21,18 +21,15 @@ function ini_status(){
           <tr>
             <th>Zeit</th>
             <th>Regensensor</th>
-            <th>temperatur Innen</th>
+            <th>temperatur Gewächshaus</th>
             <th>temperatur Außen</th>
-            <th>Luftfeuchtigkeit Innen</th>
+            <th>Luftfeuchtigkeit Gewächshaus</th>
             <th>Luftfeuchtigkeit Außen</th>
             <th>Bodenfeuchtigkeit 1</th>
             <th>Bodenfeuchtigkeit 2</th>
             <th>Bodenfeuchtigkeit 3</th>
             <th>Wasserstand</th>
             <th>Pumpe</th>
-            <th>Windgeschwindigkeit</th>
-            <th>Niederschlag (in letzter zeit)</th>
-            <th>Windrichtung</th>
           </tr>`;
         for(var i = data.length-1; i >=0;i--){
           out+='<tr>';
@@ -41,15 +38,12 @@ function ini_status(){
           out+=`<td>${data[i].gewaechshaus.temperaturInnen.toString()}</td>`;
           out+=`<td>${data[i].wetterstation.temperaturAussen.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.luftfeuchtigkeitInnen.toString()}</td>`;
-          //out+=`<td>${data[i].wetterstation.luftfeuchtigkeitAussen.toString()}</td>`;
+          out+=`<td>${data[i].wetterstation.luftfeuchtigkeitAussen.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit1.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit2.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit3.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.bewaesserung.wasserstand.toString()}</td>`;
           out+=`<td>${data[i].gewaechshaus.bewaesserung.pumpe.pumpe.toString()}</td>`;
-          out+=`<td>${data[i].wetterstation.windspeed.toString()}</td>`;
-          out+=`<td>${data[i].wetterstation.windrichtung.toString()}</td>`;
-          out+=`<td>${data[i].wetterstation.niederschlag.toString()}</td>`;
           out+='</tr>';
         }
         out+="</table>";
