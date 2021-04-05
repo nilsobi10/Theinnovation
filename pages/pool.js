63cfbd8df544e4bdc92fcbe44b7532612c4d6994
nils.obi10 <you@example.com>
2021-04-05 13:49:35 +0200

    c

--- pages/pool.js
+++ pages/pool.js
@@ -1,7 +1,35 @@
+var poolpage = document.querySelector("#pool");
+
+
 function ini_pool() {
-  let box = document.getElementById('box');
-  let page = document.getElementsByClassName('page');
-  pool.style.background = "url(backgroundpool.png) repeat center center";
+  startload();
+  google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+
+  function drawChart() {
+    var inside = [
+      ['Zeit', 'Temperatur']
+    ];
+    for (var i = 0; i < time.length; i++) {
+      inside[inside.length] = [time[i], temper[i]];
+    };
+
+    var data = google.visualization.arrayToDataTable(inside);
+
+    var options = {
+      title: 'Wassertemperatur',
+      curveType: 'function',
+      legend: {
+        position: 'bottom'
+      }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+  }
+
 }
 
 ini_pool();
