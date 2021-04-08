0c2984585a6cf43d2e84bb051de8bba7693a16e2
nils.obi10 <you@example.com>
2021-04-08 15:11:30 +0200

    b

--- pages/status.js
+++ pages/status.js
@@ -18,104 +18,155 @@ function ini_status() {
       var box = document.getElementById('box-status');
       box.innerHTML = " ";
 
+      var i = 0;
       var temperaturInnenoben = document.createElement('div');
       temperaturInnenoben.className = "datenfeld";
+      temperaturInnenoben.id = "datenfeld" + i;
+      i++;
       temperaturInnenoben.innerHTML = data[(data.length) - 1].gewaechshaus.temperaturInnenoben + " °C";
 
       var temperaturInnenunten = document.createElement('div');
       temperaturInnenunten.className = "datenfeld";
+      temperaturInnenunten.id = "datenfeld" + i;
+      i++;
       temperaturInnenunten.innerHTML = data[(data.length) - 1].gewaechshaus.temperaturInnenunten + " °C";
 
       var temperaturInnenmitte = document.createElement('div');
       temperaturInnenmitte.className = "datenfeld";
+      temperaturInnenmitte.id = "datenfeld" + i;
+      i++;
       temperaturInnenmitte.innerHTML = data[(data.length) - 1].gewaechshaus.temperaturInnenunten + " °C";
 
       var luftfeuchtigkeitInnenoben = document.createElement('div');
       luftfeuchtigkeitInnenoben.className = "datenfeld";
+      luftfeuchtigkeitInnenoben.id = "datenfeld" + i;
+      i++;
       luftfeuchtigkeitInnenoben.innerHTML = data[(data.length) - 1].gewaechshaus.luftfeuchtigkeitInnenoben + " %";
 
       var luftfeuchtigkeitInnenunten = document.createElement('div');
       luftfeuchtigkeitInnenunten.className = "datenfeld";
+      luftfeuchtigkeitInnenunten.id = "datenfeld" + i;
+      i++;
       luftfeuchtigkeitInnenunten.innerHTML = data[(data.length) - 1].gewaechshaus.luftfeuchtigkeitInnenunten + " %";
 
       var luftfeuchtigkeitInnenmitte = document.createElement('div');
       luftfeuchtigkeitInnenmitte.className = "datenfeld";
+      luftfeuchtigkeitInnenmitte.id = "datenfeld" + i;
+      i++;
       luftfeuchtigkeitInnenmitte.innerHTML = data[(data.length) - 1].gewaechshaus.luftfeuchtigkeitInnenunten + " %";
 
       var bodenfeuchtigkeit1 = document.createElement('div');
       bodenfeuchtigkeit1.className = "datenfeld";
+      bodenfeuchtigkeit1.id = "datenfeld" + i;
+      i++;
       bodenfeuchtigkeit1.innerHTML = data[(data.length) - 1].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit1 + " %";
 
       var bodenfeuchtigkeit2 = document.createElement('div');
       bodenfeuchtigkeit2.className = "datenfeld";
+      bodenfeuchtigkeit2.id = "datenfeld" + i;
+      i++;
       bodenfeuchtigkeit2.innerHTML = data[(data.length) - 1].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit2 + " %";
 
       var bodenfeuchtigkeit3 = document.createElement('div');
       bodenfeuchtigkeit3.className = "datenfeld";
+      bodenfeuchtigkeit3.id = "datenfeld" + i;
+      i++;
       bodenfeuchtigkeit3.innerHTML = data[(data.length) - 1].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit3 + " %";
 
       var wasserstand = document.createElement('div');
       wasserstand.className = "datenfeld";
+      wasserstand.id = "datenfeld" + i;
+      i++;
       wasserstand.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.wasserstand + " %";
 
       var temperaturAussen = document.createElement('div');
       temperaturAussen.className = "datenfeld";
+      temperaturAussen.id = "datenfeld" + i;
+      i++;
       temperaturAussen.innerHTML = data[(data.length) - 1].wetterstation.temperaturAussen + ' °C';
 
       var luftfeuchtigkeitAussen = document.createElement('div');
       luftfeuchtigkeitAussen.className = "datenfeld";
+      luftfeuchtigkeitAussen.id = "datenfeld" + i;
+      i++;
       luftfeuchtigkeitAussen.innerHTML = data[(data.length) - 1].wetterstation.luftfeuchtigkeitAussen + ' %';
 
       var regensensor = document.createElement('div');
       regensensor.className = "datenfeld";
+      regensensor.id = "datenfeld" + i;
+      i++;
       regensensor.innerHTML = data[(data.length) - 1].wetterstation.regensensor + ' mm';
 
       var temperaturpool = document.createElement('div');
       temperaturpool.className = "datenfeld";
+      temperaturpool.id = "datenfeld" + i;
+      i++;
       temperaturpool.innerHTML = data[(data.length) - 1].pool + ' °C';
 
       var entfernungssensor = document.createElement('div');
       entfernungssensor.className = "datenfeld";
+      entfernungssensor.id = "datenfeld" + i;
+      i++;
       entfernungssensor.innerHTML = data[(data.length) - 1].gewaechshaus.dach.entfernungssensor + ' cm';
 
       var motor = document.createElement('div');
       motor.className = "datenfeld";
+      motor.id = "datenfeld" + i;
+      i++;
       motor.innerHTML = data[(data.length) - 1].gewaechshaus.dach.motor + ' Sekunden';
 
       var ventil1 = document.createElement('div');
       ventil1.className = "datenfeld";
+      ventil1.id = "datenfeld" + i;
+      i++;
       ventil1.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.pumpe.ventil1;
 
       var ventil2 = document.createElement('div');
       ventil2.className = "datenfeld";
+      ventil2.id = "datenfeld" + i;
+      i++;
       ventil2.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.pumpe.ventil2;
 
       var ventil3 = document.createElement('div');
       ventil3.className = "datenfeld";
+      ventil3.id = "datenfeld" + i;
+      i++;
       ventil3.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.pumpe.ventil3;
 
       var ventil4 = document.createElement('div');
       ventil4.className = "datenfeld";
+      ventil4.id = "datenfeld" + i;
+      i++;
       ventil4.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.pumpe.ventil4;
 
       var pumpe = document.createElement('div'); //wie lange zuletzt gelaufeb
       pumpe.className = "datenfeld";
+      pumpe.id = "datenfeld" + i;
+      i++;
       pumpe.innerHTML = data[(data.length) - 1].gewaechshaus.bewaesserung.pumpe.ventil4;
 
       var temperaturzentrum = document.createElement('div');
       temperaturzentrum.className = "datenfeld";
+      temperaturzentrum.id = "datenfeld" + i;
+      i++;
       temperaturzentrum.innerHTML = data[(data.length) - 1].gewaechshaus.temperaturzentrum + " °C";
 
       var luftfeuchtigkeitzentrum = document.createElement('div');
       luftfeuchtigkeitzentrum.className = "datenfeld";
+      luftfeuchtigkeitzentrum.id = "datenfeld" + i;
+      i++;
       luftfeuchtigkeitzentrum.innerHTML = data[(data.length) - 1].gewaechshaus.luftfeuchtigkeitzentrum + " %";
 
       var zeit = document.createElement('div');
       zeit.className = "datenfeld";
+      zeit.id = "datenfeld" + i;
+      i++;
       zeit.innerHTML = data[(data.length) - 1].time;
 
       var benachrichtigung = document.createElement('div');
       benachrichtigung.className = "datenfeld";
+      benachrichtigung.id = "datenfeld" + i;
+      i++;
       benachrichtigung.innerHTML = data[(data.length) - 1].benachrichtigung;
 
 
@@ -145,43 +196,26 @@ function ini_status() {
       box.appendChild(zeit);
       box.appendChild(benachrichtigung);
 
-      stopload();
-      /*
-      var temp = data.length - 1
-      var out = '<div class="ich">' + data[temp].time.toString() + '</div>';
-      var out = `<table style="width:100%">
-          <tr>
-            <th>Zeit</th>
-            <th>Regensensor</th>
-            <th>temperatur Gewächshaus</th>
-            <th>temperatur Außen</th>
-            <th>Luftfeuchtigkeit Gewächshaus</th>
-            <th>Luftfeuchtigkeit Außen</th>
-            <th>Bodenfeuchtigkeit 1</th>
-            <th>Bodenfeuchtigkeit 2</th>
-            <th>Bodenfeuchtigkeit 3</th>
-            <th>Wasserstand</th>
-            <th>Pumpe</th>
-          </tr>`;
-      for (var i = data.length - 1; i >= 0; i--) {
-        out += '<tr>';
-        out += `<td>${data[i].time.toString()}</td>`;
-        out += `<td>${data[i].wetterstation.regensensor.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.temperaturInnen.toString()}</td>`;
-        out += `<td>${data[i].wetterstation.temperaturAussen.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.luftfeuchtigkeitInnen.toString()}</td>`;
-        out += `<td>${data[i].wetterstation.luftfeuchtigkeitAussen.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit1.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit2.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.bodenfeuchtigkeit.bodenfeuchtigkeit3.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.bewaesserung.wasserstand.toString()}</td>`;
-        out += `<td>${data[i].gewaechshaus.bewaesserung.pumpe.pumpe.toString()}</td>`;
+      for (var j = 0; i > j; j++) {
 
-        out += '</tr>';
+        var details = document.getElementsByClassName('datenfeld');
+        var content = document.createElement('div');
+        content.id = "details" + j;
+        content.style.display = "none";
+        content.innerHTML = " ha";
+        details.onclick = () => {
+          if (content.style.display === 'none') {
+            content.style.display = 'block';
+          } else {
+            content.style.display = 'none'
+          }
+        };
+        details.appendChild(content);
+
       }
-      out += "</table>";*/
 
-      //statuspage.innerHTML = out;
+      stopload();
+
     })
     .catch(error => {
       stopload();
